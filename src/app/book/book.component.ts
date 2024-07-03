6313f07289eaf98b556c8ee32d75a821847234c0
Gianluca Coccimiglio <gianlucacoccimiglio.lavoro@gmail.com>
2024-07-03 11:21:08 +0200

    v2

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,20 +1,30 @@
-import { Component, Input } from '@angular/core';
-import { Book } from '../model/Book';
-import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Book } from '../model/Book';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [MatCardModule,CommonModule],
+  imports: [MatCardModule,CommonModule,MatIconModule,MatButtonModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
 export class BookComponent 
 {
-  //il valore della var content arriva dal apdre= Booklist
-  @Input() content!:Book; 
-};
 
-  @Input() content!:Book;
+  @Input() content!:Book; //dichiariamo ciò che il padre manda al figlio
+
+  //Ora dichiariamo un evento che il figlio può far scaturire
+  //Output permette di parlare con il padre, EventEmitter emette un evento
+  @Output() deleteEvent:EventEmitter<number> = new EventEmitter<number>();
+
+  delete()
+  {
+    this.deleteEvent.emit(this.content.id);
+  }
+
+
 }
